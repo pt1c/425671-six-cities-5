50c6fe3044dc126f286fc8651c493c68f3efd2e1
Kirill Savinov <kirill@savinov.me>
2023-10-19 15:33:16 +0300

    IoC + DI

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -2,12 +2,15 @@ import { Config } from './config.interface.js';
 import { Logger } from '../logger/index.js';
 import { config } from 'dotenv';
 import { configRestSchema, RestSchema } from './rest.schema.js';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/index.js';
 
+@injectable()
 export class RestConfig implements Config<RestSchema> {
   private readonly config: RestSchema;
 
   constructor(
-     private readonly logger: Logger
+     @inject(Component.Logger) private readonly logger: Logger
   ) {
     const parsedOutput = config();
 
