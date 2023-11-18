d4c27868bb17811d3418983c3f24f20be2e55263
pt1c <ravenzone@mail.ru>
2023-11-18 11:59:08 +0300

    + errors handler

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -4,3 +4,4 @@ export * from './controller/controller.interface.js';
 export * from './controller/base-controller.abstract.js';
 export * from './exception-filter/exception-filter.interface.js';
 export * from './exception-filter/app-exception-filter.js';
+export * from './errors/index.js';
